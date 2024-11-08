86bb099c0abe23751c7f150cc7ecc5a2b8a1534e
SomratIIT <101197949+SomratIIT@users.noreply.github.com>
2024-11-08 13:06:19 +0600

    Script tag er fitor function likha

--- js/events.js
+++ js/events.js
@@ -1,1 +1,12 @@
 console.log('This is Separted JS file');
+
+
+
+function makeRed(){
+    document.body.style.backgroundColor='red';
+}
+
+function makeTomato()
+{
+    document.body.style.backgroundColor='tomato';
+}
