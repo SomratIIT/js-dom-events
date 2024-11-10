0210216e29b847581a607accad6ff6233e816b19
SomratIIT <101197949+SomratIIT@users.noreply.github.com>
2024-11-10 10:57:55 +0600

    Multiple use of onclick and addeventlistener

--- js/events.js
+++ js/events.js
@@ -10,7 +10,7 @@ function makeTomato()
 {
     document.body.style.backgroundColor='tomato';
 }
-// Option 02
+// Option 02 
 const makeBlueButton = document.getElementById('make-blue');
        makeBlueButton.onclick=makeBlue  ;
 
@@ -19,3 +19,42 @@ const makeBlueButton = document.getElementById('make-blue');
         document.body.style.backgroundColor='blue';
        
        }
+
+
+       const makeBlackColor = document.getElementById('make-black');
+       makeBlackColor.onclick = makeBlack  ;
+
+       function makeBlack()
+       {
+        document.body.style.backgroundColor='black';
+       }
+
+
+ // Option 03
+ const makePurpleButton= document.getElementById('make-purple');
+
+ makePurpleButton.onclick = function makePurple()
+ {
+     document.body.style.backgroundColor='purple';
+ }
+
+//  Option 04
+const pinkButton = document.getElementById('make-pink');
+       pinkButton.addEventListener('click',makePink);
+
+       function makePink()
+       {
+        document.body.style.backgroundColor='pink';
+       }
+// Option 04 Another
+const makeGreenButton = document.getElementById('make-green');
+makeGreenButton.addEventListener('click', function makeGreen()
+{
+document.body.style.backgroundColor='green';
+});
+
+// Option 04 final   Important
+document.getElementById('make-white').addEventListener('click', function makeWhite()
+{
+document.body.style.backgroundColor='white';
+});
